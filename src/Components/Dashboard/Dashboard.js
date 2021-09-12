05d2f75a8f0dd805d61992b6dd67657a515eda32
Simbamon <sbhan9511@gmail.com>
2021-09-12 17:14:25 +0900

    Table work in progress

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -5,7 +5,16 @@ import { ArrowDownward, ArrowUpward, Person, ShowChart, QuestionAnswer, Assignme
 import { withTranslation } from 'react-i18next';
 import { FiberManualRecord } from '@material-ui/icons'
 import Client1KR from '../../images/client1KR.jpg';
+import Client2KR from '../../images/client2KR.jpg';
+import Client3KR from '../../images/client3KR.jpg';
+import Client4KR from '../../images/client4KR.jpg';
+import Client5KR from '../../images/client5KR.jpg';
 import Client1EN from '../../images/client1EN.jpg';
+import Client2EN from '../../images/client2EN.jpg';
+import Client3EN from '../../images/client3EN.jpg';
+import Client4EN from '../../images/client4EN.jpg';
+import Client5EN from '../../images/client5EN.jpg';
+
 
 
 export class Dashboard extends Component {
@@ -113,16 +122,15 @@ export class Dashboard extends Component {
                                     <td>1024</td>
                                     <td>
                                         <Client>
-                                            {/* <img src={localStorage.getItem('i18nextLng')==="en" ?imageEN : imageKR}/> */}
                                             <ClientPic img = {localStorage.getItem('i18nextLng')==="ko" ?Client1KR : Client1EN} alt='discovery workshop'></ClientPic>
                                             <ClientName>
                                                 <div style = {{display: "table-cell", verticalAlign: "middle"}}>
-                                                    <p style = {{fontSize: "0.85rem"}}>{t('navbar.1')}</p>
+                                                    <p style = {{fontSize: "0.85rem"}}>{t('client.1')}</p>
                                                 </div>
                                             </ClientName>
                                         </Client>
                                     </td>
-                                    <td>example@example.com</td>
+                                    <td>{t('client.1e')}</td>
                                     <td>주택담보</td>
                                     <td>
                                         <Purchase>
@@ -138,8 +146,17 @@ export class Dashboard extends Component {
                                 </tr>
                                 <tr>
                                     <td>1031</td>
-                                    <td>adsf</td>
-                                    <td>example@example.com</td>
+                                    <td>
+                                        <Client>
+                                            <ClientPic img = {localStorage.getItem('i18nextLng')==="ko" ?Client2KR : Client2EN} alt='discovery workshop'></ClientPic>
+                                            <ClientName>
+                                                <div style = {{display: "table-cell", verticalAlign: "middle"}}>
+                                                    <p style = {{fontSize: "0.85rem"}}>{t('client.2')}</p>
+                                                </div>
+                                            </ClientName>
+                                        </Client>
+                                    </td>
+                                    <td>{t('client.2e')}</td>
                                     <td>저축보험</td>
                                     <td>
                                         <Purchase>
@@ -155,8 +172,17 @@ export class Dashboard extends Component {
                                 </tr>
                                 <tr>
                                     <td>1055</td>
-                                    <td>adsf</td>
-                                    <td>example@example.com</td>
+                                    <td>
+                                        <Client>
+                                            <ClientPic img = {localStorage.getItem('i18nextLng')==="ko" ?Client3KR : Client3EN} alt='discovery workshop'></ClientPic>
+                                            <ClientName>
+                                                <div style = {{display: "table-cell", verticalAlign: "middle"}}>
+                                                    <p style = {{fontSize: "0.85rem"}}>{t('client.3')}</p>
+                                                </div>
+                                            </ClientName>
+                                        </Client>
+                                    </td>
+                                    <td>{t('client.3e')}</td>
                                     <td>연금보험</td>
                                     <td>
                                         <Purchase>
@@ -172,8 +198,17 @@ export class Dashboard extends Component {
                                 </tr>
                                 <tr>
                                     <td>1202</td>
-                                    <td>adsf</td>
-                                    <td>example@example.com</td>
+                                    <td>
+                                        <Client>
+                                            <ClientPic img = {localStorage.getItem('i18nextLng')==="ko" ?Client4KR : Client4EN} alt='discovery workshop'></ClientPic>
+                                            <ClientName>
+                                                <div style = {{display: "table-cell", verticalAlign: "middle"}}>
+                                                    <p style = {{fontSize: "0.85rem"}}>{t('client.4')}</p>
+                                                </div>
+                                            </ClientName>
+                                        </Client>
+                                    </td>
+                                    <td>{t('client.4e')}</td>
                                     <td>자녀보험</td>
                                     <td>
                                         <Purchase>
@@ -189,8 +224,17 @@ export class Dashboard extends Component {
                                 </tr>
                                 <tr>
                                     <td>1729</td>
-                                    <td>adsf</td>
-                                    <td>example@example.com</td>
+                                    <td>
+                                        <Client>
+                                            <ClientPic img = {localStorage.getItem('i18nextLng')==="ko" ?Client5KR : Client5EN} alt='discovery workshop'></ClientPic>
+                                            <ClientName>
+                                                <div style = {{display: "table-cell", verticalAlign: "middle"}}>
+                                                    <p style = {{fontSize: "0.85rem"}}>{t('client.5')}</p>
+                                                </div>
+                                            </ClientName>
+                                        </Client>
+                                    </td>
+                                    <td>{t('client.5e')}</td>
                                     <td>연금보험</td>
                                     <td>
                                         <Purchase>
