9b31904e3975f8c076faeb8c1d7c747684b1cecb
Simbamon <sbhan9511@gmail.com>
2021-09-25 22:40:56 +0900

    Client page

--- src/Components/Clients/Client1.js
+++ src/Components/Clients/Client1.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react'
+import { ClientWrap, ClientTitle, ClientBoxWrapper, ClientBox, Client, ClientPic } from './Client_element'
 import { withTranslation } from 'react-i18next';
+import Client1KR from '../../images/client1KR.jpg';
+import Client1EN from '../../images/client1EN.jpg';
 
 export class Client1 extends Component {
     render() {
         return (
             <>
-                test
+                <ClientWrap>
+                    <ClientTitle>
+                        Client
+                    </ClientTitle>
+                    <ClientBoxWrapper>
+                        <ClientBox>
+                            <Client>
+                                <ClientPic img = {localStorage.getItem('i18nextLng')==="ko" ?Client1KR : Client1EN} alt='discovery workshop'></ClientPic>
+                            </Client>
+                        </ClientBox>
+                    </ClientBoxWrapper>
+                </ClientWrap>
             </>
         )
     }
--- src/Components/Clients/Client_element.js
+++ src/Components/Clients/Client_element.js
@@ -1,1 +1,50 @@
 import styled from 'styled-components'
+
+export const ClientWrap = styled.div`
+    background-color: #F3F3F4;
+    min-height: calc(100vh - 50px);
+    height: 100%;
+    padding: 25px;
+`
+
+export const ClientTitle = styled.div`
+    padding: 10px;
+    font-size: 1.7rem;
+    font-weight: bold;
+    line-height: 1.7rem;
+`
+
+export const ClientBoxWrapper = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    align-items: center;
+    align-content: center;
+`
+
+export const ClientBox = styled.div`
+    flex: 1;
+    margin : 10px;
+    padding: 15px;
+    height: 100%;
+    border-radius: 10px;
+    white-space: nowrap;
+    background-color: white;
+    box-shadow: 13px 15px 55px -13px rgba(0,0,0,0.36);
+`
+
+export const Client = styled.div`
+    display: flex;
+    
+`
+
+export const ClientPic = styled.div`
+    background-image: url(${props => props.img});
+    background-size: cover;
+    width: 150px;
+    border-radius: 10%;
+    height: 150px;
+    text-decoration: none;
+ 
+`
